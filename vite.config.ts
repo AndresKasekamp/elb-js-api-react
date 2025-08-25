427ba3c5c76295c430d8ac3e7b57a163a8fc18ae
Andres Kasekamp <andreskasekamp@gmail.com>
2025-08-25 16:40:43 +0300

    elb-js

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/elb-js-api-react-e717a7/"
+  // base: "/elb-js-api-react-e717a7/"
 })
