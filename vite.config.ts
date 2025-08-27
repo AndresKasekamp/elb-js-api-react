49706d124987bec936b89def9c6095a38fd2a687
Andres Kasekamp <andreskasekamp@gmail.com>
2025-08-27 09:37:26 +0300

    changed based

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/arcgis/elb-js-api-react/"
+  base: "/arcgis/front-end/elb-js-api-react/"
 })
